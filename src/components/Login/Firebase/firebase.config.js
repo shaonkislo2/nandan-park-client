d136568949ed4d09500b9bb4d6ecb82c3c99814e
shaonkislo <shaon6340@agmail.com>
2021-10-30 00:41:55 +0600

    12.42

---
+++ src/components/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATBWQp_8LH44vLU2pBkJiV5UHV3a3tbec",
+    authDomain: "nandan-park.firebaseapp.com",
+    projectId: "nandan-park",
+    storageBucket: "nandan-park.appspot.com",
+    messagingSenderId: "421452242378",
+    appId: "1:421452242378:web:790b8943271e3352378333"
+  };
+  export default firebaseConfig;
