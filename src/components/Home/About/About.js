8d5d95aaf4957c4847eeddfe970bcba30a46df69
shaonkislo <shaon6340@agmail.com>
2021-10-30 20:26:27 +0600

    8.30pm

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -3,43 +3,10 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-			<div className="mt-5">
-				<h1 className="text-primary">Write Us</h1>
-				<div className="contact-form p-3">
-					<div className="row  d-flex justify-content-center ">
-						<div className="col-md-6 mt-5">
-							<input
-								className="input-field"
-								type="text"
-								placeholder="Enter Your Name"
-							/>
-							<br />
-							<input
-								className="input-field"
-								type="Email"
-								placeholder="Enter Your Email"
-							/>
-							<br />
-							<input
-								className="input-field"
-								type="text"
-								placeholder="Write Details"
-							/>
-							<br />
-							<input className="input-button" type="submit" value="Submit" />
-						</div>
-						<div className="col-md-6">
-							<div className="contact-im">
-								<img
-									className="w-100"
-									src="https://i.ibb.co/VJdS431/Image-008.png"
-									alt=""
-								/>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			
+			<h2>This is abpout</h2>
+			<br />
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/Yczpuuu-U6M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
     );
 };
